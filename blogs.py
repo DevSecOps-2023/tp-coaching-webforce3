c71111c274e92c57a1a50b7e12c682b9c0c29833
ndouba2 <nsammy@hotmail.fr>
2023-02-24 08:59:04 +0000

    tp

---
+++ blogs.py
@@ -0,0 +1,16 @@
+```python
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+logging.basicConfig(filename='log/record.log', level=logging.DEBUG, format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(messa>
+
+@app.route('/blogs')
+def blog():
+    app.logger.info('Info level log')
+    app.logger.warning('Warning level log')
+    return f"Welcome to the Blog"
+
+app.run(host='localhost', debug=True)
+```
